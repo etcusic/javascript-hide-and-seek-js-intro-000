0e123080491290f1e5388001e66dc0eeea4934e5
Eli Cusic <tntitans31@gmail.com>
2020-06-09 20:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function getFirstSelector(selector){
-
+  var main = document.getElementsByTagName('main')
+  for (var i = 0; i < main.length; i++){
+    if (main[i] === selector){
+      return main[i]
+    }
+  }
 }
 
 function nestedTarget(){
